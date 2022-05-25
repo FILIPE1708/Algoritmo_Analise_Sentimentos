dc5b1b65b0b1c836a961970a860547ea4f649a9b
Filipe <lipecavalcante17@hotmail.com>
2022-05-25 01:28:18 -0300

    Atualização de código

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
+from analise_sentimentos import AnaliseSentimentos
 from googletrans import Translator
 from textblob import TextBlob
 
+analise = AnaliseSentimentos()
 frase = input('Digite uma frase: ')
 
 tradutor = Translator()
-fraseTraduzida = tradutor.translate(frase)
+fraseTraduzida = tradutor.translate(frase, src='auto', dest='pt')
 fraseTratada = TextBlob(fraseTraduzida.text)
-resposta = fraseTratada.sentiment.polarity
 
-if resposta < 0:
+print(f'Tradução: {fraseTratada}')
+resultado = analise.avalia(str(fraseTratada))
+print(resultado)
+
+if resultado['score'] < 0:
     print('Frase negativa')
 
-elif resposta == 0:
+elif resultado['score'] == 0:
     print('Frase neutra')
 
-elif resposta > 0:
+elif resultado['score'] > 0:
     print('Frase positiva')
